0aa88b88f8f02ef041576efc9e9e825dd5d886b2
Nicotorreno <nicotorreno2@alumnos.cei.es>
2024-01-16 10:16:03 +0100

    cambios

--- vlogsurf/vlogsurf.js
+++ vlogsurf/vlogsurf.js
@@ -1,20 +1,41 @@
-let abrirModal = document.getElementById("abrirModal");
-let cierraModal = document.getElementById("cierraModal");
-function agregaEvento() {
-  abrirModal.addEventListener("click", abrir);
-  cierraModal.addEventListener("click", cerrar);
+
+let cierraModal;
+let suscribirModal;
+
+function agregaEvento(){
+
+cierraModal = document.getElementById("cierraModal");
+suscribirModal = document.getElementById("suscribir");
+
+
+cierraModal.addEventListener("click", cerrar);
+suscribirModal.addEventListener("click", suscribir);
 }
 
-function abrir() {
-  let modal = document.getElementById("idModal");
+function abrir(){
+    let modal = document.getElementById("idModal");
 
-  modal.style.display = "flex";
+    modal.style.display = "flex";
 }
 
-function cerrar() {
-  let modal = document.getElementById("idModal");
-  modal.style.display = "none";
+function cerrar(){
+    let modal = document.getElementById("idModal");
+    modal.style.display = "none";
 }
 
-window.onload = agregaEvento;
+function suscribir(){
+    let correoModal = document.getElementById("correoModal");
+    let passModal = document.getElementById("passModal");
+    if(correoModal.value == ''|| passModal.value == '')  {
+        alert('Para suscribirte debes escribir un correo y una contraseña');
+    }else{
+        alert('Te has suscrito correctamente');
+    }
+}
+window.onload = function(){
+    agregaEvento();
+    setTimeout(abrir, 2000);
+};
+
+
 
